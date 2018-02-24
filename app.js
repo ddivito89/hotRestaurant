c979f1c8e9032f61d8c8b8f7fe0cbc9bd01dc771
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-02-24 13:36:05 -0600

    some routing

--- app.js
+++ app.js
@@ -5,7 +5,25 @@ var path = require("path")
 var app = express()
 var PORT = 3000;
 
-var openbTables = []
+var tables = [
+  {
+    tableId: 1,
+    occupant: 'null'
+  }, {
+    tableId: 2,
+    occupant: 'null'
+  }, {
+    tableId: 3,
+    occupant: 'null'
+  }, {
+    tableId: 4,
+    occupant: 'null'
+  }, {
+    tableId: 5,
+    occupant: 'null'
+  }
+]
+
 var waitList = []
 
 app.use(bodyParser.urlencoded({extended: false}))
@@ -15,16 +33,38 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"))
 })
 
-app.get("/api/all", function(req, res) {
-  res.send("some data")
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"))
 })
 
+app.get("/api/tables", function(req, res) {
+  res.json(tables)
+})
+
+app.get("/api/waitlist", function(req, res) {
+  res.json(waitList)
+})
 
 app.post("/reserve", function(req, res) {
-  console.log(req.body)
-  res.send("res received")
-})
 
+  var reservation = req.body
+  var seated = false;
+
+  for (var x = 0; x < tables.length; x++) {
+    if (tables[x].occupant === 'null') {
+      tables[x].occupant = reservation
+      seated = true
+      break;
+    }
+  }
+
+  if (seated === false) {
+    waitList.push(reservation)
+  }
+
+  res.sendFile(path.join(__dirname, "tables.html"))
+
+})
 
 app.listen(PORT, function() {
   console.log("app listening on port " + PORT)
